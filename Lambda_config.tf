ec11cb09d9bb62668a0deb73591e1fecce984ee2
Julen Miner <mjulen@live.com>
2020-11-13 10:30:49 +0100

    added lambda policy and role

---
+++ Lambda_config.tf
@@ -0,0 +1,46 @@
+resource "aws_iam_role_policy" "policy_for_lambda" {
+  name = "policy_for_lambda"
+  role = aws_iam_role.role_for_lambda.id
+
+  policy = <<-EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": ["s3:ListBucket"],
+            "Resource": ["arn:aws:s3:::${var.bucket_name}"]
+        },
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:PutObject",
+                "s3:GetObject",
+                "s3:DeleteObject"
+            ],
+            "Resource": ["arn:aws:s3:::${var.bucket_name}/*"]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_role" "role_for_lambda" {
+  name = "role_for_lambda"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "lambda.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
