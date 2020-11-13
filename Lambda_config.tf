0fd40710504ff8a38d80bb69e6691e62a9e49c00
Julen Miner <mjulen@live.com>
2020-11-13 11:07:54 +0100

    added lambda trigger

--- Lambda_config.tf
+++ Lambda_config.tf
@@ -59,3 +59,23 @@ resource "aws_lambda_function" "image_resizer_lambda" {
     }
   }
 }
+
+# Create S3 bucket notification (trigger)
+resource "aws_lambda_permission" "allow_bucket" {
+  statement_id  = "AllowExecutionFromS3Bucket"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.image_resizer_lambda.arn
+  principal     = "s3.amazonaws.com"
+  source_arn    = aws_s3_bucket.images_bucket.arn
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.images_bucket.id
+
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.image_resizer_lambda.arn
+    events              = ["s3:ObjectCreated:*"]
+    filter_prefix       = "original-images/"
+  }
+  depends_on = [aws_lambda_permission.allow_bucket]
+}
